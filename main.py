bb4b6efa497764d5e7854be8a726b285b5b458c2
Wanajaporn <p.wanajaporn@gmail.com>
2023-11-29 11:27:20 +0700

    add height

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ class Mycalculator:
         self.label = tk.Label(self.root, text="Hello DIP01", font= ("Arial", 18))
         self.label.pack()
 
-        self.button = tk.Button(self.root, text="AC")
+        self.button = tk.Button(self.root, text="AC", height=4)
         self.button.place(x=20, y=50)
 
-        self.button = tk.Button(self.root, text="+/-")
+        self.button = tk.Button(self.root, text="+/-", height=4 )
         self.button.place(x=50, y=50)
 
-        self.button = tk.Button(self.root, text="%")
+        self.button = tk.Button(self.root, text="%", height=4)
         self.button.place(x=80, y=50)
 
-        self.button = tk.Button(self.root, text="7")
+        self.button = tk.Button(self.root, text="7", height=4)
         self.button.place(x=20, y=80)
 
-        self.button = tk.Button(self.root, text="8")
+        self.button = tk.Button(self.root, text="8", height=4)
         self.button.place(x=50, y=80)
 
-        self.button = tk.Button(self.root, text="9")
+        self.button = tk.Button(self.root, text="9", height=4)
         self.button.place(x=80, y=80)
 
 
